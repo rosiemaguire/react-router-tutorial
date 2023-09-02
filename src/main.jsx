ec5a3b6b5f949a4f2114df00080e3496f6245e20
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-09-02 13:00:04 +1000

    Navbar created and pages linked

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Import Modules
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// Import Pages
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Here we import our components
+import NavBar from "./components/NavBar.jsx";
+// Here we create our router and tell it whats pages to render at what path
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* Here we wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
